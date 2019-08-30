549e2931262b0fbf6b39bc3774a61593e6e01282
giladmaya <ms.maya.gilad@gmail.com>
2019-08-30 22:04:00 +0300

    Visualization file

--- core/training.py
+++ core/training.py
@@ -6,20 +6,12 @@ import torch
 import torch.nn as nn
 from torch import optim
 from core.lang import EOS_token, SOS_token
-import matplotlib.pyplot as plt
+
 import matplotlib.ticker as ticker
 
+from core.visualizations import show_attention, show_plot
 
 _TEACHER_FORCING_RATIO = 0.5
-
-
-def show_plot(points):
-    plt.figure()
-    fig, ax = plt.subplots()
-    # this locator puts ticks at regular intervals
-    loc = ticker.MultipleLocator(base=0.2)
-    ax.yaxis.set_major_locator(loc)
-    plt.plot(points)
 
 
 def as_minutes(s):
@@ -184,25 +176,6 @@ def evaluate_randomly(device, encoder, decoder, input_lang, output_lang, test_pa
         output_sentence = ' '.join(output_words)
         print('<', output_sentence)
         print('')
-
-
-def show_attention(input_sentence, output_words, attentions):
-    # Set up figure with colorbar
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    cax = ax.matshow(attentions.numpy(), cmap='bone')
-    fig.colorbar(cax)
-
-    # Set up axes
-    ax.set_xticklabels([''] + input_sentence.split(' ') +
-                       [f'<{EOS_token[1]}>'], rotation=90)
-    ax.set_yticklabels([''] + output_words)
-
-    # Show label at every tick
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
-
-    plt.show()
 
 
 def evaluate_and_show_attention(device, encoder, decoder, input_lang, output_lang, max_len, input_sentence):
